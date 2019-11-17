0f2a32443589df8a0078bef4b793299a66b5db3f
Shazia <n_kirmani@yahoo.com>
2019-11-17 15:02:31 +0000

    week 1

--- challenges/week1.js
+++ challenges/week1.js
@@ -1,54 +1,107 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+
+const firstChar =word.charAt(0).toUpperCase();
+
+const restOfWord=word.slice(1);
+const fullWord =firstChar + restOfWord;
+
+return fullWord;
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  return firstName.charAt (0)
+   +"." + lastName.charArt(0)
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
-}
+  let VAT = vatRate / 100
+  let totalPrice = (VAT * originalPrice) + originalPrice;
+  return Math.round(totalPrice * 100) / 100;
+} 
+
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  
+  let sale = reduction/100
+  let salePrice=originalPrice-
+  (sale * originalPrice)
+  return  Math.round(salePrice*100)/100;
 }
-
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  else if (str.length % 2 === 0) {
+    let mid = str.length / 2
+    return str.slice(mid - 1, mid + 1)
+  } else {
+    let newLength = str.length + 1
+    // eslint-disable-next-line no-undef
+    mid = newLength / 2
+    // eslint-disable-next-line no-undef
+    return str.slice(mid - 1, mid)
+  }
 }
-
+      
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word.split("").reverse().join("");
 }
+
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
+  let newArr = []
+  words.forEach(function (word) {
+    // eslint-disable-next-line no-undef
+    reversedWord = word.split("").reverse().join("");
+    // eslint-disable-next-line no-undef
+    newArr.push(reversedWord);
+  })
+  return newArr;
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  // Add your code here!
+  
+    let total = 0
+  users.forEach(function (user) {
+    const osType = user.type
+    if (osType === "Linux") {
+      total += 1;
+    }
+  })
+  return total;
 }
+
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  // Add your code here!
+let sum=0
+scores.forEach(function (score){
+  sum=sum+score
+});
+return Math.round((sum/scores.length)*100)/100
+
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-  // Add your code here!
+if (n%3=== 0 && n%5===0){
+  return "fizzbuzz"
+}else if( n %3 !== 0  && n%5 !==0){
+return n
+}else if (n% 3===0){
+  return "fizz"
+}else if (n%5===0){
+  return"buzz"
+}
+
 }
 
 module.exports = {
--- challenges/week4.js
+++ challenges/week4.js
@@ -1,27 +1,56 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
-}
+  
+    let newN = [];
+    for(let i=0; i<nums.length; i++){
+      if(nums[i] < 1){
+        newN.push(nums[i]);
+      }
+    }
+    return newN;
+  }
+  
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
-}
+  //return array of names begin with s
+ 
+ 
+
+
+
+
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  const verb=[];
+  for (let i= 0; i<words.length; i++){
+  const word=words[i];
+  const first3Chars = word.slice(0,3);
+  if (first3Chars=== "to "){
+    verb.push(word);
+  }
 }
+return verbs;
+}
+
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
-}
+  
+
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  const cities=[]
+ for ( let i= 0;i<users.length; i++){
+  const user=users[i];
+  const displayName=user.data.city.displayName;cities.push(displayName);
+ }
+ return cities;
+
+
 }
 
 function getSquareRoots(nums) {
@@ -37,7 +66,15 @@ function findSentencesContaining(sentences, str) {
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+
+  const largestEdges=[];
+for(let i=0;i<triangles.length;i++){
+const triangle =triangle[i];
+triangle.sort(function (a, b){return b-a
+});
+const largestEdge=triangle[0]
+largestEdges.push(largestEdge);
+return largestEdges;
 }
 
 module.exports = {
