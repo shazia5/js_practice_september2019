98b39da1267cf5cb044ea0f90aa6bb66246ce783
Shazia <n_kirmani@yahoo.com>
2019-11-25 20:05:59 +0000

    week2 done

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,12 +1,18 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
-
+  
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if(person.city === "Manchester"){
+    return true;
+  }else{
+    return false;
+  }
+  //return person.city === "Manchester" ? true : false;
 }
+
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
@@ -22,8 +28,15 @@ function getBusNumbers(people) {
 //return Math.ceil(people/40);
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let sheep = 0;
+  for(let key in arr){
+    if(arr[key] === "sheep"){
+      sheep++;
+    }
+  }
+  return sheep;
 }
+
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
@@ -31,12 +44,11 @@ function hasMPostCode(person) {
 //  const firstLetter=person.address.postCode[0];return firstLetter==="M" &&person.address.city ==="Manchester" 
 
 //without city
-const firstLetter=person.address.postCode[0];
-const secondLetter=person.address.postCode[1];
-const isAlphabeticCharaccter=isNaN(Number(secondLetetr)}
-return firstLetter==="M" && !isAlphabeticCharaccter;
-
+let personCity = person.address.city;
+  let personPostCode = person.address.postCode;
+  return personCity === "Manchester" && personPostCode.slice(0,1) === "M";
 }
+
 
 
 module.exports = {
